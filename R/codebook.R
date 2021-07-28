7bf6439957eab12afc175967ab3bcbc01ee855cd
Nick Williams <ntwilliams.personal@gmail.com>
2021-07-28 12:28:07 -0400

    doc updates.

--- R/codebook.R
+++ R/codebook.R
@@ -3,13 +3,14 @@
 #' @param data The data to label.
 #' @param path The path to YAML code book.
 #' @param label Should column names also be renamed according to code book labels?
-#' @param as_labelled Should the code book be applied using the labelled package?
+#' @param to_labelled Should the code book be applied using the labelled package?
 #' @param .include An optional character vector of column names to
 #'  apply the code book to.
 #' @param .exclude An optional character vector of column names to not apply
 #'  the code book to. Ignored if \code{.include} is specified.
 #'
-#' @return The data with the columns found in the code book labeled.
+#' @return An updated copy of \code{data}.
+#'
 #' @export
 #'
 #' @examples
@@ -21,7 +22,7 @@
 #' )
 #' codebook(ex, system.file("codebook.yml", package = "codebook"))
 codebook <- function(data, path = "dictionary.yml", label = FALSE,
-                     as_labelled = FALSE, .include = NULL, .exclude = NULL) {
+                     to_labelled = FALSE, .include = NULL, .exclude = NULL) {
   check_path(path)
   codebook <- yaml::read_yaml(path)
 
